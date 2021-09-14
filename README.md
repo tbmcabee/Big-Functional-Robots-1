0ee3eb3cfb5aeccfeba312ed0c7854baa0a9940a
jaidenbm0127 <jaidenbm0127@gmail.com>
2021-09-14 19:17:14 -0400

    Added modID, and put it in any necessary places and created starting structure for package names

---
+++ README.md
@@ -0,0 +1,4 @@
+# Big Functional Robots
+TBD
+
+Version 0.0.1 (Pre-Alpha)
---
+++ src/main/java/com/bfr/robots/Robots.java
@@ -0,0 +1,41 @@
+package com.bfr.robots;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("robots")
+public class Robots
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Robots() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        // Register the setup method for modloading
+        bus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
