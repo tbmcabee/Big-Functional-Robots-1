49a718e7e7fd4626dc29eab358f1bb2c0ac039eb
jaidenbm0127 <jaidenbm0127@gmail.com>
2021-09-14 16:17:37 -0400

    Added modID, and put it in any necessary places and created starting structure for package names

--- README.md
+++
@@ -1,4 +0,0 @@
-# Big Functional Robots
-TBD
-
-Version 0.0.1 (Pre-Alpha)
--- build/tmp/expandedArchives/forge-1.17.1-37.0.44_mapped_official_1.17.1-sources.jar_edb5c2833d6811137e0e0d202883e154/net/minecraft/world/level/levelgen/synth/SurfaceNoise.java
+++
@@ -1,5 +0,0 @@
-package net.minecraft.world.level.levelgen.synth;
-
-public interface SurfaceNoise {
-   double getSurfaceNoiseValue(double p_75484_, double p_75485_, double p_75486_, double p_75487_);
-}
