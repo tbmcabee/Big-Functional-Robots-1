f284eff68cc5973835c5c6e913ad519edb852991
jaidenbm0127 <jaidenbm0127@gmail.com>
2021-09-27 23:59:28 -0400

    Implemented world generation

---
+++ src/main/java/com/bfr/robots/world/BFRFeatures.java
@@ -0,0 +1,21 @@
+package com.bfr.robots.world;
+
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.data.worldgen.Features;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import com.bfr.robots.world.*;
+
+public class BFRFeatures {
+	public static void addOres(BiomeGenerationSettings.Builder builder, Biome.BiomeCategory biomeCategory) {
+        int stageOre = GenerationStep.Decoration.UNDERGROUND_ORES.ordinal();
+        
+        
+        
+        builder.addFeature(stageOre, () -> BFRConfiguredFeatures.ORE_BLOCK);
+    }
+}
