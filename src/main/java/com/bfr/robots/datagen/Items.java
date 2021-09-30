638e211f0885af8e42ec920430c225846eccd174
Tallon <tallon.mcabee@gmail.com>
2021-09-29 22:58:55 -0400

    Data generation comments

--- src/main/java/com/bfr/robots/datagen/Items.java
+++ src/main/java/com/bfr/robots/datagen/Items.java
@@ -9,15 +9,23 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
+//This class is meant to represent the specific item model JSON generation code that will be 
+//utilized within DataGenerators in order to create its specific JSON model. 
 public class Items extends ItemModelProvider 
 {
-
+	
+	//The public constructor that will be utilized within the DataGenerators class 
+	//in order to create a class object that can be interacted within within the class' code. 
+	//This constructor is inherited from the ItemModelProvider class (Tallon)
 	public Items(DataGenerator generator, ExistingFileHelper existingFileHelper) 
 	{
 		super(generator, Robots.MOD_ID, existingFileHelper);
 		// TODO Auto-generated constructor stub
 	}
 	
+	//This method will register the texture location with a specified preset JSON model to utilize within the game engine.
+	//This is specifically for generating a JSON model to represent the OreItem class' registered item within the game 
+	//when the player is "holding" the item. (Tallon)
 	protected void registerModels()
 	{
 		singleTexture(OreItem.ORE_INGOT.get().getRegistryName().getPath(),
