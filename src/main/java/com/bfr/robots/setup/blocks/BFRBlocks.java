891c5660ccc99d702e4a32c583e6d3c8cc897ee9
jaidenbm0127 <jaidenbm0127@gmail.com>
2021-10-18 19:46:38 -0400

    Restructured code

---
+++ src/main/java/com/bfr/robots/setup/blocks/BFRBlocks.java
@@ -0,0 +1,52 @@
+package com.bfr.robots.setup.blocks;
+
+import java.util.function.Supplier;
+
+import com.bfr.robots.Robots;
+import com.bfr.robots.setup.Registration;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.RegistryObject;
+
+@Mod.EventBusSubscriber(modid = Robots.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class BFRBlocks {
+	
+		//This initialization method creates a RegistryObject Object utilizing the registerBlock and registerBlockItem helper methods.
+		//The purpose of this method is to create a forward facing registration item for the entire block, including its block
+		//registration and item registration, for the game even bus to interact with. This initialization will also dictate 
+		//the properties that the block item will follow including the base mining strength (mining resistance) and a method to 
+		//establish the block will only drop its loot table for the correctly set out Tool (Defined in Block Tags under Data). (Tallon)
+		public static final RegistryObject<Block> ORE_BLOCK = registerBlock("ore_block", 
+				() -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.TERRACOTTA_LIGHT_GRAY).strength(5.0f, 6.0f)
+						.sound(SoundType.METAL).requiresCorrectToolForDrops()));
+		
+		//This helper method will create a registration item based upon a generic supplier T and a string name.
+		//This method will utilize a Block Supplier and a string name to register the block and create a 
+		//logically symmetric item to represent the registered block. At the end of this method it will return the 
+		//completed block registration. (Tallon)
+		private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block)
+		{
+	        RegistryObject<T> toReturn = Registration.BLOCKS.register(name, block);
+	        registerBlockItem(name, toReturn);
+	        return toReturn;
+	    }
+		
+		//This helper method will create a registration item based upon a generic supplier T and a string name. 
+		//The purpose of this method is to create an item for the block to utilize as an asset for the player to hold.
+		//If this asset did not exist, the block would not be intereactable for player to utilize within the game world. (Tallon)
+		private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block)
+		{
+			//This statement 
+	        Registration.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+	    }
+		
+		//This method will be utilized to register the BlockOre's ORE_BLOCK asset into the Registration Class' event bus (Tallon)
+		public static void register() {}
+}
---
+++ src/main/java/com/bfr/robots/setup/items/BFRItems.java
@@ -0,0 +1,57 @@
+package com.bfr.robots.setup.items;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.RegistryObject;
+
+import com.bfr.robots.Robots;
+import com.bfr.robots.items.ArmorTier;
+import com.bfr.robots.items.ItemArmor;
+import com.bfr.robots.items.ItemAxe;
+import com.bfr.robots.items.ItemHoe;
+import com.bfr.robots.items.ItemPickaxe;
+import com.bfr.robots.items.ItemShovel;
+import com.bfr.robots.items.ItemSword;
+import com.bfr.robots.items.OreItem;
+import com.bfr.robots.items.ToolTier;
+import com.bfr.robots.setup.Registration;
+
+@Mod.EventBusSubscriber(modid = Robots.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class BFRItems {
+	
+	//This statement creates the RegistryObject reference for the Ore Ingot utilizing Registration.ITEMS.register method in order
+	//to create the Ore Ingot utilizing Minecraft's preset ITEM Group class (Tallon)
+	public static final RegistryObject<Item> ORE_INGOT = Registration.ITEMS.register("ore_ingot", () ->
+		new OreItem(new Item.Properties()));
+	
+	public static final RegistryObject<Item> NOVA_CRYSTAL_HELMET = Registration.ITEMS.register("nova_crystal_helmet", 
+			() -> new ItemArmor(ArmorTier.NOVA_CRYSTAL, EquipmentSlot.HEAD, (new Item.Properties())));
+	
+	public static final RegistryObject<Item> NOVA_CRYSTAL_CHESTPLATE = Registration.ITEMS.register("nova_crystal_chestplate", 
+			() -> new ItemArmor(ArmorTier.NOVA_CRYSTAL, EquipmentSlot.CHEST, (new Item.Properties())));
+	
+	public static final RegistryObject<Item> NOVA_CRYSTAL_LEGGINGS = Registration.ITEMS.register("nova_crystal_leggings", 
+			() -> new ItemArmor(ArmorTier.NOVA_CRYSTAL, EquipmentSlot.LEGS, (new Item.Properties())));
+	
+	public static final RegistryObject<Item> NOVA_CRYSTAL_BOOTS = Registration.ITEMS.register("nova_crystal_boots", 
+			() -> new ItemArmor(ArmorTier.NOVA_CRYSTAL, EquipmentSlot.FEET, (new Item.Properties())));
+	
+	public static final RegistryObject<Item> NOVA_CRYSTAL_AXE = Registration.ITEMS.register("nova_crystal_axe",
+            () -> new ItemAxe(ToolTier.NOVA_CRYSTAL, 6.0F, -3.2F, (new Item.Properties())));
+	
+	public static final RegistryObject<Item> NOVA_CRYSTAL_HOE = Registration.ITEMS.register("nova_crystal_hoe",
+            () -> new ItemHoe(ToolTier.NOVA_CRYSTAL, 0, -3.0F, (new Item.Properties())));
+	
+    public static final RegistryObject<Item> NOVA_CRYSTAL_PICKAXE = Registration.ITEMS.register("nova_crystal_pickaxe",
+            () -> new ItemPickaxe(ToolTier.NOVA_CRYSTAL, 1, -2.8F, (new Item.Properties())));
+    
+    public static final RegistryObject<Item> NOVA_CRYSTAL_SHOVEL = Registration.ITEMS.register("nova_crystal_shovel",
+            () -> new ItemShovel(ToolTier.NOVA_CRYSTAL, 1.5F, -3.0F, (new Item.Properties())));
+    
+    public static final RegistryObject<Item> NOVA_CRYSTAL_SWORD = Registration.ITEMS.register("nova_crystal_sword",
+            () -> new ItemSword(ToolTier.NOVA_CRYSTAL, 3, -2.4F, (new Item.Properties())));
+	
+	public static void register() {}
+	
+}
